4464d693cdf4c7c07f1ccc383992d4f757f5e27e
yjshin-cloud <yjshin.cloud@gmail.com>
2025-08-30 19:15:52 +0900

    test

--- config.js
+++ config.js
@@ -13,10 +13,10 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "yjshin-cloud",
     company: "weniv",
     position: "CEO",
-    img: "img/user/profile-licat.png",
+    img: "img/user/profile-yjshin-cloud.png.png",
   },
 ];
 
